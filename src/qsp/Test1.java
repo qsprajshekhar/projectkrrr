15cc06f03ac707e03110a01490d7511d5aa6a99b
qsprajshekhar <rajshekhar419@gmail.com>
2022-05-23 21:37:42 +0530

    dshfkdw

--- src/qsp/Test1.java
+++ src/qsp/Test1.java
@@ -4,13 +4,7 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-			String c; 
-			System.out.println();
-=======
-		int a;
 
->>>>>>> branch 'Mybranch' of https://github.com/qsprajshekhar/projectkrrr.git
 	}
 
 }
--- src/qsp/Test2.java
+++ src/qsp/Test2.java
@@ -3,15 +3,12 @@ package qsp;
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-=======
+
 		//fhjjksddbwhd
 		//kfehkjewdsbashhebfnsdmnaddsfve
 		//faejhkjheefhwbwhfvnbj
 		String ad;
-		String vvvvvv;
->>>>>>> branch 'Mybranch' of https://github.com/qsprajshekhar/projectkrrr.git
+
 
 	}
 
